61555226dbda3edb405f4594fe9a42ef7ab80955
Wiput Pootong <wiput.pootong@gmail.com>
2018-04-18 00:01:48 +0700

    :bug: Fix curl

--- RaspberryPi/main.c
+++ RaspberryPi/main.c
@@ -83,13 +83,13 @@ int read_pir(void)
 {
 	FILE *fp;
 
-	CURL *curl;
-	CURLcode res;
+	// CURL *curl;
+	// CURLcode res;
 
-	curl_mime *form = NULL;
-	curl_mimepart *field = NULL;
-	struct curl_slist *headerlist = NULL;
-	static const char buf[] = "Expect:";
+	// curl_mime *form = NULL;
+	// curl_mimepart *field = NULL;
+	// struct curl_slist *headerlist = NULL;
+	// static const char buf[] = "Expect:";
 
 	curl_global_init(CURL_GLOBAL_ALL);
 
@@ -119,35 +119,78 @@ int read_pir(void)
 				printf("Process 2 : Command has started\n");
 
 				curl = curl_easy_init();
+				CURL *curl;
+				CURLcode res;
+
+				struct curl_httppost *formpost = NULL;
+				struct curl_httppost *lastptr = NULL;
+				struct curl_slist *headerlist = NULL;
+				static const char buf[] = "Expect:";
+
+				curl_global_init(CURL_GLOBAL_ALL);
+
+				/* Fill in the file upload field */
+				curl_formadd(&formpost,
+							 &lastptr,
+							 CURLFORM_COPYNAME, "photo",
+							 CURLFORM_FILE, fp,
+							 CURLFORM_END);
+
+				curl = curl_easy_init();
+				/* initialize custom header list (stating that Expect: 100-continue is not
+     wanted */
+				headerlist = curl_slist_append(headerlist, buf);
 				if (curl)
 				{
-					form = curl_mime_init(curl);
-
-					field = curl_mime_addpart(form);
-					curl_mime_name(field, "photo");
-					curl_mime_filedata(field, fp);
-
-					headerlist = curl_slist_append(headerlist, buf);
-
+					/* what URL that receives this POST */
 					curl_easy_setopt(curl, CURLOPT_URL, "http://example.com/examplepost.cgi");
 
-					curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
+					curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
 
-					/* Get result */
+					/* Perform the request, res will get the return code */
 					res = curl_easy_perform(curl);
-
+					/* Check for errors */
 					if (res != CURLE_OK)
 						fprintf(stderr, "curl_easy_perform() failed: %s\n",
 								curl_easy_strerror(res));
 
+					/* always cleanup */
 					curl_easy_cleanup(curl);
 
-					curl_mime_free(form);
-
+					/* then cleanup the formpost chain */
+					curl_formfree(formpost);
+					/* free slist */
 					curl_slist_free_all(headerlist);
-
-					printf("Process 2 : Command successfully run\n");
 				}
+				// if (curl)
+				// {
+				// 	form = curl_mime_init(curl);
+
+				// 	field = curl_mime_addpart(form);
+				// 	curl_mime_name(field, "photo");
+				// 	curl_mime_filedata(field, fp);
+
+				// 	headerlist = curl_slist_append(headerlist, buf);
+
+				// 	curl_easy_setopt(curl, CURLOPT_URL, "http://example.com/examplepost.cgi");
+
+				// 	curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
+
+				// 	/* Get result */
+				// 	res = curl_easy_perform(curl);
+
+				// 	if (res != CURLE_OK)
+				// 		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				// 				curl_easy_strerror(res));
+
+				// 	curl_easy_cleanup(curl);
+
+				// 	curl_mime_free(form);
+
+				// 	curl_slist_free_all(headerlist);
+
+				// 	printf("Process 2 : Command successfully run\n");
+				// }
 				else
 				{
 					printf("Process 2 : Failed to run curl\n");
