a6a7391bdfd569e032117b11e2b5b4d790a13232
Wiput Pootong <wiput.pootong@gmail.com>
2018-04-21 00:55:47 +0700

    :bug: Fix run command and rm command

--- RaspberryPi/main.c
+++ RaspberryPi/main.c
@@ -201,8 +201,6 @@ int read_pir(void)
 			snprintf(owner, 200, "%s", getenv("TELEGRAM_USER"));
 			snprintf(accesskey, 200, "%s", getenv("ACCESSKEY"));
 
-			printf("%s\n%s\n%s\n", device_id, owner, accesskey);
-
 			curl_global_init(CURL_GLOBAL_ALL);
 
 			/* Fill in the file upload field */
@@ -263,7 +261,7 @@ int read_pir(void)
 				/* free slist */
 				curl_slist_free_all(headerlist);
 
-				system("rm /tmp/motion.jpg");
+				system("rm -f /tmp/motion.jpg");
 
 				printf("Process 2 : Command successfully run\n");
 			}
